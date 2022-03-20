c26c547615a3a0e601872c0f6445059900de0a9e
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-20 09:12:11 +0300

    Change App.tsx, completed homeworks task

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,18 @@ import './App.css';
 function App() {
     return (
         <div>
-            This is APP components!
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
+}
+
+function AppTitle() {
+    return (
+        <>This is APP components!</>
+    )
 }
 
 function Rating() {
@@ -32,14 +39,28 @@ function Star() {
 function Accordion() {
     return (
         <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
     )
 }
+
+function AccordionTitle () {
+    return(
+        <h3>Menu</h3>
+    )
+}
+
+function AccordionBody () {
+    return(
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+
 
 export default App;
