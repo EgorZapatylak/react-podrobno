2ce70baf8074b2f9f4e526c22bae23e717e5d2d7
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-20 15:51:35 +0300

    Changed file UncontrolledRating.tsx

--- src/components/UncontrolledRating/UncontrolledRating.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.tsx
@@ -5,8 +5,7 @@ type RatingPropsType = {
 }
 
 type StarPropsType = {
-    setValue: (value:number)=> void
-    value: 1 | 2 | 3 | 4 | 5
+    setValue: ()=> void
     selected: boolean
 }
 
@@ -16,17 +15,17 @@ export function UncontrolledRating(props: RatingPropsType) {
 
     return (
         <div>
-            <Star selected={value > 0} setValue={setValue} value={1}/>
-            <Star selected={value > 1} setValue={setValue} value={2}/>
-            <Star selected={value > 2} setValue={setValue} value={3}/>
-            <Star selected={value > 3} setValue={setValue} value={4}/>
-            <Star selected={value > 4} setValue={setValue} value={5}/>
+            <Star selected={value > 0} setValue={()=>{setValue(1)}}/>
+            <Star selected={value > 1} setValue={()=>{setValue(2)}}/>
+            <Star selected={value > 2} setValue={()=>{setValue(3)}}/>
+            <Star selected={value > 3} setValue={()=>{setValue(4)}}/>
+            <Star selected={value > 4} setValue={()=>{setValue(5)}}/>
         </div>
     )
 }
 
 function Star(props: StarPropsType) {
-    return <span onClick={()=>{props.setValue(props.value)}}>
+    return <span onClick={()=>{props.setValue()}}>
         {props.selected ? <b> star</b> : "  star"}
             </span>
 }
