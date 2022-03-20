dd57bede0b16dbb38fb0308fce0d6255a19efa0d
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-20 08:44:47 +0300

    Create new project

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is APP components!
     </div>
   );
+}
+
+function Rating () {
+    return(
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    )
+}
+
+function Accordion() {
+    return(
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
