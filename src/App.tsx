d034098137be35781a1b95fd4bce2460ac1129d1
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-20 15:51:53 +0300

    Changed file App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Accordion} from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 import {OnOff} from "./components/OnOff/OnOff";
 import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
 import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
+import {UncontrolledOnOff} from "./components/UncontrolledOnOff/UncontrolledOnOff";
 
 function App() {
+
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(3)
+    let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(true)
+    let [switchOn, setSwitchOn] = useState(true);
+
     return (
         <div className={"App"}>
-            {/*<PageTitle title={"This is APP components!"}/>*/}
-            {/*Article 1*/}
-            {/*<Rating value={3}/>*/}
             <UncontrolledAccordion titleValue={"Menu"}/>
             <UncontrolledRating/>
-            <OnOff/>
+            <UncontrolledOnOff />
 
-            <Accordion titleValue={"User"} collapsed={false}/>
-            <Rating value={3}/>
-            <OnOff/>
-            {/*<Accordion titleValue={"Menu"} collapsed={true}/>*/}
-            {/*<Accordion titleValue={"User"} collapsed={false}/>*/}
-            {/*Article 2*/}
-            {/*<Rating value={0}/>*/}
-            {/*<Rating value={1}/>*/}
-            {/*<Rating value={2}/>*/}
-            {/*<Rating value={3}/>*/}
-            {/*<Rating value={4}/>*/}
-            {/*<Rating value={5}/>*/}
+            <Accordion titleValue={"User"} collapsed={accordionCollapsed} onChange={()=>{setAccordionCollapsed(!accordionCollapsed)}}/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+            <OnOff on={switchOn} onChange={setSwitchOn}/>
+
         </div>
     );
 }
