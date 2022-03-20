3ad6194eb108bbf5dd451f8b7b567af704f119ab
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-20 12:55:07 +0300

    Changed init value of components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,38 @@ import React from 'react';
 import './App.css';
 import {Accordion} from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {OnOff} from "./components/OnOff/OnOff";
+import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 
 function App() {
     return (
-        <div>
-            <PageTitle title={"This is APP components!"}/>
-            Article 1
-            <Rating value={3}/>
-            <Accordion titleValue={"Menu"} collapsed={true}/>
-            <Accordion titleValue={"User"} collapsed={false}/>
-            Article 2
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+        <div className={App}>
+            {/*<PageTitle title={"This is APP components!"}/>*/}
+            {/*Article 1*/}
+            {/*<Rating value={3}/>*/}
+
+            <UncontrolledAccordion titleValue={"Menu"} />
+            <UncontrolledAccordion titleValue={"User"} />
+
+            <UncontrolledRating/>
+
+            {/*<Accordion titleValue={"Menu"} collapsed={true}/>*/}
+            {/*<Accordion titleValue={"User"} collapsed={false}/>*/}
+            {/*Article 2*/}
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+            <OnOff/>
+            <OnOff/>
         </div>
     );
 }
 
-function PageTitle(props:any) {
+function PageTitle(props: any) {
     return (
         <h2>{props.title}</h2>
     )
