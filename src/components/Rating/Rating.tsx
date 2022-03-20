038705dc0689050e8533a99922a340e081ccc6ee
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-20 15:51:13 +0300

    Changed file UncontrolledRating.tsx

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 
+export type RatingValueType= 0 | 1 | 2 | 3 | 4 | 5;
+
+
 type RatingPropsType = {
-    value: number
+    value: RatingValueType
+    onClick: (value: RatingValueType) => void
 }
 
 type StarPropsType = {
     selected: boolean
+    value: RatingValueType
+    onClick: (value: RatingValueType) => void
 }
 
 export function Rating(props: RatingPropsType) {
 
-    let selected = true;
-
     return (
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <Star selected={props.value > 0} onClick={props.onClick} value={1}/>
+            <Star selected={props.value > 1} onClick={props.onClick} value={2}/>
+            <Star selected={props.value > 2} onClick={props.onClick} value={3}/>
+            <Star selected={props.value > 3} onClick={props.onClick} value={4}/>
+            <Star selected={props.value > 4} onClick={props.onClick} value={5}/>
         </div>
     )
 }
 
 function Star(props: StarPropsType) {
-    return props.selected ?  <span>  <b>star</b></span> : <span>  star</span>
+    return <span onClick={()=>{props.onClick(props.value)}}>
+        {props.selected ? <b> star</b> : "  star"}
+            </span>
 }
